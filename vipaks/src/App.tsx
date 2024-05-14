0f149b624ecf9dc048f74e7c3802dc13b3cd9919
ArseniyKhal <asmn@mail.ru>
2024-05-14 20:13:33 +0500

    CRA

---
+++ vipaks/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p>	header</p>
+			</header>
+			<main>
+			<p>main</p>
+			</main>
+		</div>
+	);
+}
+
+export default App;
